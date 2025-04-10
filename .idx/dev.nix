a5eb9fcb028cc7d1c109108622978224d5853867
imaas.wynder <imaas.wynder@gmail.com>
2025-04-10 13:04:42 +0000

    On branch SciEyes_extractor  Your branch is ahead of 'origin/SciEyes_extractor' by 1 commit.  Changes to be committed: 	modified:   .idx/dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,10 +1,23 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
+  name = "riskguard-capture-dev";
+
+  buildInputs = with pkgs; [
+    nodejs_18
+    nodePackages.npm
+    # React development tools (adjust as needed)
+    vscode-extensions.dbaeumer.vscode-eslint
+    vscode-extensions.esbenp.prettier-vscode
+
+    # Database service
+    mysql80
+  ];
+
 shellHook = ''
-  # export HOME=/home/user
+   export PATH="/nix/store/i52w1ch1cb2ia9yh0brivwyl1crayw49-mysql-8.0.37/bin:$PATH"
 
-  # Add MySQL bin directory to PATH
-  export PATH="/nix/store/i52w1ch1cb2ia9yh0brivwyl1crayw49-mysql-8.0.37/bin:$PATH"
-
-  # Initialize MySQL data directory
   mkdir -p /tmp/mysql
   mysql_install_db --datadir=/tmp/mysql --basedir=/nix/store/i52w1ch1cb2ia9yh0brivwyl1crayw49-mysql-8.0.37
 
@@ -19,4 +32,5 @@ shellHook = ''
   echo "Run 'npm install' to install project dependencies."
   echo "Run 'npm start' to start the client-side app."
   echo "In another terminal, run 'npm run build:server' and 'npm run start:server' to start the server."
-''
+'';
+}
